bd8ce68cc8bb244ecf9cc0f84acbe776070b28ad
Jhon <169020516+cloudocean32@users.noreply.github.com>
2025-07-18 16:00:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 const IP_FILE = path.join(__dirname, 'ips.txt');
+const PROXY_FILE = path.join(__dirname, 'proxies.txt');
 
 // Middleware untuk logging request
 app.use((req, res, next) => {
@@ -28,6 +29,25 @@ app.get('/ips', (req, res) => {
         res.json({
             count: ips.length,
             ips: ips
+        });
+    });
+});
+
+app.get('/proxies', (req, res) => {
+    fs.readFile(PROXY_FILE, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading Proxy file:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        // Memisahkan PROXY berdasarkan baris dan menghapus yang kosong
+        const proxies = data.split('\n')
+                       .map(proxy => proxy.trim())
+                       .filter(proxy => proxy.length > 0);
+
+        res.json({
+            count: proxies.length,
+            proxies: proxies
         });
     });
 });
