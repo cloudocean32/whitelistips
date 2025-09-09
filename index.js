1e796ef43d93e8376935cd7dd7a815a9becddb55
Jhon <169020516+cloudocean32@users.noreply.github.com>
2025-09-10 02:17:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
 const PORT = 3000;
 const IP_FILE = path.join(__dirname, 'ips.txt');
 const PROXY_FILE = path.join(__dirname, 'proxies.txt');
 
-// Middleware untuk logging request
 app.use((req, res, next) => {
     console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
     next();
 });
 
-// Endpoint untuk mendapatkan list IP
 app.get('/ips', (req, res) => {
     fs.readFile(IP_FILE, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading IP file:', err);
             return res.status(500).json({ error: 'Internal Server Error' });
         }
-
-        // Memisahkan IP berdasarkan baris dan menghapus yang kosong
         const ips = data.split('\n')
                        .map(ip => ip.trim())
                        .filter(ip => ip.length > 0);
@@ -39,8 +34,6 @@ app.get('/proxies', (req, res) => {
             console.error('Error reading Proxy file:', err);
             return res.status(500).json({ error: 'Internal Server Error' });
         }
-
-        // Memisahkan PROXY berdasarkan baris dan menghapus yang kosong
         const proxies = data.split('\n')
                        .map(proxy => proxy.trim())
                        .filter(proxy => proxy.length > 0);
@@ -52,12 +45,10 @@ app.get('/proxies', (req, res) => {
     });
 });
 
-// Endpoint root
 app.get('/', (req, res) => {
     res.send('Welcome to IP List Service. Access /ips to get the IP list.');
 });
 
-// Jalankan server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
